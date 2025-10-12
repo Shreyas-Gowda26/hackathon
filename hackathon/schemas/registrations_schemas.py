1e0e9de9625b69483f034533111f724bf7e9dc58
Shreyas Gowda <shreyasg2526@gmail.com>
2025-10-12 19:02:13 +0530

    Updated my schemas

--- hackathon/schemas/registrations_schemas.py
+++ hackathon/schemas/registrations_schemas.py
@@ -1,15 +1,1 @@
-from pydantic import BaseModel,EmailStr
-from datetime import datetime
-
-class Registration_base(BaseModel):
-    hackathon_id :str
-    user_id:str
-    team_name:str
-
-class Registration_Response(BaseModel):
-    id:str
-    registered_at : datetime
-
-    class Config:
-        orm_mode = True
-
+In
