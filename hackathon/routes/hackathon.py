29300d22d4ce52426bdffd74589d8f02f28f0424
Shreyas Gowda <shreyasg2526@gmail.com>
2025-10-12 20:49:50 +0530

    Finished with hackathon router

---
+++ hackathon/routes/hackathon.py
@@ -0,0 +1,55 @@
+from fastapi import APIRouter,Depends,HTTPException,status
+from database import hackathon_collection
+from hackathon.schemas.hackathon_schemas import HackathonBase,HackathonResponse
+from typing import List
+from datetime import datetime
+from bson import ObjectId
+router = APIRouter(
+    prefix="/hackathons",
+    tags=["Hackathons"]
+)
+
+@router.post("/",response_model=HackathonResponse)
+def create_hackathon(hackathon:HackathonBase):
+    hackathon_dict = hackathon.model_dump()
+    res = hackathon_collection.insert_one(hackathon_dict)
+    hackathon_dict["created_at"]= datetime.utcnow()
+    hackathon_dict["id"]=str(res.inserted_id)
+    return HackathonResponse(id=str(res.inserted_id),**hackathon_dict)
+
+@router.get("/",response_model=List[HackathonResponse])
+def get_all_hackathons():
+    hackathons = []
+    for hackathon in hackathon_collection.find():
+        data = {k : hackathon[k] for k in hackathon if k!="_id"}
+        hackathons.append(HackathonResponse(id=str(hackathon["id"]),**data))
+    return hackathons
+
+@router.get("/{hackathon_id}",response_model=HackathonResponse)
+def get_hackathon_by_id(hackathon_id:str):
+    hackathon = hackathon_collection.find_one({"_id":ObjectId(hackathon_id)})
+    if not hackathon:
+        raise HTTPException(status_code=404,detail="Hackathon not found")
+    data = {k: hackathon[k] for k in hackathon if k!="_id"}
+    return HackathonResponse(id=str(hackathon["_id"]),**data)
+
+@router.put("/{hackathon_id}",response_model=HackathonResponse)
+def update_hackathon(hackathon_id:str,hackathon:HackathonBase):
+    existing_hackathon = hackathon_collection.find_one({"_id":ObjectId(hackathon_id)})
+    if not existing_hackathon:
+        raise HTTPException(status_code =404,detail="Hackathon not found")
+    updated_hackathon = hackathon.model_dump()
+    hackathon_collection.update_one({"_id":ObjectId(hackathon_id)},{"set":updated_hackathon})
+    updated_hackathon["id"]=hackathon_id
+    return HackathonResponse(id=hackathon_id,**updated_hackathon)
+
+@router.delete("/{hackathon_id}",response_model=HackathonResponse)
+def delete_hackathon(hackathon_id:str):
+    hackathon = hackathon_collection.find_one({"_id":ObjectId(hackathon_id)})
+    if not hackathon:
+        raise HTTPException(status_code=404,detail="Hackathon not found")
+    hackathon_collection.delete_one({"_id":ObjectId(hackathon_id)})
+    data={k: hackathon[k] for k in hackathon if k!="_id"}
+    return HackathonResponse(id=str(hackathon["_id"]),**data)
+
+
--- hackathon/schemas/registrations_schemas.py
+++ hackathon/schemas/registrations_schemas.py
@@ -1,1 +1,15 @@
-In
+from pydantic import BaseModel,EmailStr
+from datetime import datetime
+
+class Registration_base(BaseModel):
+    hackathon_id :str
+    user_id:str
+    team_name:str
+
+class Registration_Response(BaseModel):
+    id:str
+    registered_at : datetime
+
+    class Config:
+        orm_mode = True
+
